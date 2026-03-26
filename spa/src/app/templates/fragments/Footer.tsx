36fa88a65f0187611a4740485c92830d97dc31a1
Kirk Alyn Santos <kirkalyn13@gmail.com>
2026-03-25 22:34:01 -0400

    feat: update footer alignments

--- spa/src/app/templates/fragments/Footer.tsx
+++ spa/src/app/templates/fragments/Footer.tsx
@@ -7,20 +7,21 @@ const year = new Date().getFullYear();
 const Footer = ({ content }: { content: MgnlContent }) => {
   return (
     <>
-      <div className='grid grid-cols-3 gap-12'>
+      <div className='grid md:grid-cols-3 sm:grid-cols-1 md:gap-12 sm:gap-2'>
         <a
           href='https://dailyplanetdc.com/'
           target='_blank'
           rel='noopener noreferrer'
         >
           <img
-            className='py-6 max-w-64'
+            className='py-6 md:max-w-64 text-center'
             src='/daily-planet-logo-inverted.png'
             alt='Daily Planet Logo'
           />
         </a>
         <div className='text-center text-white py-6'>
-          <p>© {year} Daily Planet. Delivering truth, justice, and clean UI. All rights belong to DC Comics.</p>
+          <p> © {year} Daily Planet. Delivering truth, justice, and clean UI. </p>
+          <p> All rights belong to DC Comics.</p>
         </div>
       </div>
     </>
