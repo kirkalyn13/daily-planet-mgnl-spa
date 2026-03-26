556e152945c29a6bc72f984d81468f0be0b8a90f
Kirk Alyn Santos <kirkalyn13@gmail.com>
2026-03-26 06:38:19 -0400

    feat: init article list page

--- spa/src/magnolia.config.ts
+++ spa/src/magnolia.config.ts
@@ -4,6 +4,7 @@ import Home from './app/templates/pages/Home/Home';
 import General from './app/templates/pages/General/General';
 import Folder from './app/templates/pages/Folder/Folder';
 import Article from './app/templates/pages/Article/Article';
+import ArticleList from './app/templates/pages/ArticleList/ArticleList';
 
 import Hero from './app/templates/components/Hero/Hero';
 import LinkList from './app/templates/components/LinkList/LinkList';
@@ -18,6 +19,7 @@ export const config: MagnoliaConfig = {
     'daily-planet-lm:pages/general': General,
     'daily-planet-lm:pages/folder': Folder,
     'daily-planet-lm:pages/article': Article,
+    'daily-planet-lm:pages/article-list': ArticleList,
 
     // Component Templates
     'daily-planet-lm:components/hero': Hero,
